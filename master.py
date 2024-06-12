972357497f6ca054264754a5a162df2ed154b868
wenx <xw0902@gmail.com>
2024-06-12 09:58:20 +0200

    added figures

--- master.py
+++
@@ -1,9 +0,0 @@
-
-
-
-
-condition = torch.tensor([365,25,2,2]).to(device)
-
-gen = generate_sequence(decoder_model, start_token=SOS_token, max_length=100, condition_vector=condition, device=device, temperature=0.5)[:-1]
-decoded_string = sequence_to_string(gen, input_lang.index2char)
-print("Decoded sequence:", decoded_string)
